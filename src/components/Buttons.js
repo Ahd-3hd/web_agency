de01d6230599871bbad2b5d3b338e01919ac9d65
Ahd <dafuq.ahd@gmail.com>
2020-07-01 18:34:24 +0300

    create rectangular button component

---
+++ src/components/Buttons.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+export const RectButton = styled.button`
+  padding: 0.5rem 2rem;
+  margin: 2rem;
+  border: none;
+  background: #2ab640;
+  color: #ffffff;
+  box-shadow: 0px 0px 8px rgba(81, 81, 81, 0.2);
+`
