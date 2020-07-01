73c1cd2ac6581c8c3762f431fa9b61bd8fdb3560
Ghassan <ghassan.maslamani@gmail.com>
2020-07-01 11:01:55 +0300

    installing styledCompoenets

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Zaat.dev`,
+    description: `Zaat.dev For developing digital soultion, developing webapps, tasks autmation, SEO fast website, creating state of the art (AI) smart soultion and more `,
+    author: `@zaatdev`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -18,13 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Zaat.dev`,
+        short_name: `Zaat`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#4FCC5D`,
+        theme_color: `#4FCC5D`,
+        display: `standalone`,
+        icon: `src/images/zaat_logo_square.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
