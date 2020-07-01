cb253c694c6520e0e283f891d7dfbdfcec78d3cd
Ahd <dafuq.ahd@gmail.com>
2020-07-01 19:21:28 +0300

    nothing changed here

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,12 @@ import React from "react"
 import Layout from "../components/layout"
 import Image from "../components/Zaat_logo"
 import SEO from "../components/seo"
-
 const IndexPage = () => (
   <Layout>
-     <SEO title="Home"/>
-     <h2>Zatt.dev</h2>
-     <span>Sections goes here</span>
-     <Image/>
+    <SEO title="Home" />
+    <h2>Zatt.dev</h2>
+    <span>Sections goes here</span>
+    <Image />
   </Layout>
 )
 
