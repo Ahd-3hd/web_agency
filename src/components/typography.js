7f72b0e6e427f328bd803ab62624a22fd78e6e89
Ahd <dafuq.ahd@gmail.com>
2020-07-01 18:36:18 +0300

    create several typography components

---
+++ src/components/typography.js
@@ -0,0 +1,49 @@
+import styled from "styled-components"
+
+export const H1 = styled.h1`
+  font-weight: 700;
+  font-size: 4rem;
+  color: ${({ color }) => color};
+`
+export const H2 = styled.h2`
+  font-weight: 700;
+  font-size: 3rem;
+  color: ${({ color }) => color};
+`
+export const H3 = styled.h3`
+  font-weight: 700;
+  font-size: 2rem;
+  color: ${({ color }) => color};
+`
+export const H4 = styled.h4`
+  font-weight: 700;
+  font-size: 1.5rem;
+  color: ${({ color }) => color};
+`
+export const H5 = styled.h5`
+  font-weight: 700;
+  font-size: 1.2rem;
+  color: ${({ color }) => color};
+`
+export const Body1 = styled.p`
+  font-size: 1.2rem;
+  color: ${({ color }) => color};
+  font-weight: 400;
+`
+export const Body2 = styled.p`
+  font-weight: 500;
+  font-size: 1rem;
+  color: ${({ color }) => color};
+`
+export const Subtitle = styled.p`
+  font-size: 1rem;
+  color: ${({ color }) => color};
+  font-weight: 600;
+`
+export const ButtonText = styled.p`
+  font-size: 1rem;
+  color: ${({ color }) => color};
+  font-weight: 600;
+  text-transform: uppercase;
+  margin: 0;
+`
