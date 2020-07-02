f8cbab4854129af61c87d3c289d4295165244f19
Ahd <dafuq.ahd@gmail.com>
2020-07-02 12:37:57 +0300

    change the animation of RectButton so that it elevates on hover, and when clicked elevation decreases in addition to material like button click effect

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 export const RectButton = styled.button`
+  display: flex;
   padding: 0.5rem 2rem;
   margin: 2rem;
   border: none;
@@ -8,28 +9,43 @@ export const RectButton = styled.button`
   color: #ffffff;
   box-shadow: 0px 0px 8px rgba(81, 81, 81, 0.2);
   cursor: pointer;
-  transition: box-shadow 0.3s ease;
+  transition: box-shadow 0.2s ease;
   overflow: hidden;
+  outline: lightgreen;
   :hover {
     box-shadow: 0px 5px 15px rgba(81, 81, 81, 0.5);
   }
   position: relative;
-  &::after {
+  ::after {
     content: '';
-    width: 100%;
-    height: 100%;
-    background: #26c0b722;
+    background: #54cf61;
+    width: 200px;
+    height: 200px;
     position: absolute;
-    top: 0;
+    border-radius: 200px;
+    align-self: center;
+    justify-self: center;
     left: 0;
-    transition: transform 0.5s ease;
-    mix-blend-mode: screen;
+    top: 0;
     transform: scale(0);
-    border-radius: 100%;
     transform-origin: center;
   }
-  &:hover ::after {
-    transform: scale(2);
+  :active {
+    ::after {
+      animation: animate_button 0.3s ease 0s forwards;
+    }
+    box-shadow: 0px 1px 4px rgba(81, 81, 81, 0.5);
+  }
+
+  @keyframes animate_button {
+    from {
+      transform: scale(0);
+      opacity: 0;
+    }
+    to {
+      transform: scale(2);
+      opacity: 1;
+    }
   }
 `
 
