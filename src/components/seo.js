4709169f57467bd363a74dd9c14493e423d76555
Ahd <dafuq.ahd@gmail.com>
2020-07-07 00:10:20 +0300

    add link tag for google fonts poppins in helmet component

--- src/components/seo.js
+++ src/components/seo.js
@@ -68,7 +68,12 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
   )
 }
 
