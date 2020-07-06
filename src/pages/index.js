1625d60ef49da8b5849977ef95547cdc9b56b070
Ahd <dafuq.ahd@gmail.com>
2020-07-06 23:47:57 +0300

    remove Image and Span components that comes with boilerplate, and add Contact component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Layout from '../components/layout'
-import Image from '../components/Zaat_logo'
+// import Image from '../components/Zaat_logo'
 import SEO from '../components/seo'
+import Contact from '../components/Contact'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h2>Zatt.dev</h2>
-    <span>Sections goes here</span>
-    <Image />
+    <Contact />
   </Layout>
 )
 
