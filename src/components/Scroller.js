e7e27d3ea172f7f325aa7b559c1188612d2be177
Ahd <dafuq.ahd@gmail.com>
2020-07-16 21:24:01 +0300

    remove dummy divs

--- src/components/Scroller.js
+++ src/components/Scroller.js
@@ -11,24 +11,6 @@ const Scroller = () => (
     <Slide>
       <Contact />
     </Slide>
-    <Slide>
-      <div
-        style={{
-          width: '100%',
-          height: '100%',
-          background: 'red',
-        }}
-      />
-    </Slide>
-    <Slide>
-      <div
-        style={{
-          width: '100%',
-          height: '100%',
-          background: 'blue',
-        }}
-      />
-    </Slide>
   </FullPage>
 )
 
