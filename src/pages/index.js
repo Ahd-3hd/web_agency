95f14f5693317fbc16a1052c711b93e1085ae7b3
Ahd <dafuq.ahd@gmail.com>
2020-07-17 23:56:09 +0300

    move chatbutton and whatsapp button to Buttons.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,12 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
 import Layout from '../components/layout'
-// import Image from '../components/Zaat_logo'
 import SEO from '../components/seo'
 import Chat from '../components/Chat'
-import { RoundAnchorButton, RoundButton } from '../components/Buttons'
+import { ChatButton, WhatsAppButton } from '../components/Buttons'
 import WhatsappIcon from '../static/WhatsappIcon.svg'
 import CloseIcon from '../static/CloseIcon.svg'
 import MessageIcon from '../static/MessageIcon.svg'
 import Scroller from '../components/Scroller'
-
-const ChatButton = styled(RoundButton)`
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  z-index: 999;
-`
-
-const WhatsAppButton = styled(RoundAnchorButton)`
-  position: fixed;
-  right: 0.5rem;
-  bottom: 0px;
-  z-index: 999;
-  animation: animatewa 0.3s ease forwards;
-  @keyframes animatewa {
-    from {
-      bottom: 0px;
-    }
-    to {
-      bottom: 60px;
-    }
-  }
-`
 
 const IndexPage = () => {
   const [toggleChat, setToggleChat] = useState(false)
