3006384d781cbf9fe54a8b3dbabf4243e7bd7f5b
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-07-21 23:35:54 +0300

    chore: build skewed card

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,3 +8,14 @@ export const Card = styled.div`
   box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.2);
   border-radius: 0.4rem;
 `
+
+export const PolygonCard = styled.div`
+  margin: 1rem;
+  padding: 1rem;
+  background: #ffffff;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2);
+  box-sizing: border-box;
+  border-radius: 0.4rem;
+  transform: skew(${({ rotation = '20deg' }) => rotation});
+`
