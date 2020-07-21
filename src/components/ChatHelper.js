53ac11e11b8f1a336e6d6fd4b8a9b75c3280ff9d
Ahd <dafuq.ahd@gmail.com>
2020-07-21 21:14:10 +0300

    tweak the review and submit component

--- src/components/ChatHelper.js
+++ src/components/ChatHelper.js
@@ -1,41 +1,59 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
-import { H4, Td } from './typography'
-
+import styled from 'styled-components'
+import { colors } from '../utils'
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 0.7rem;
+  * {
+    margin: 0;
+  }
+  color: ${colors.gray};
+`
+const Title = styled.p`
+  margin: 0;
+`
+const FieldsContainer = styled.div``
+const FieldGroup = styled.div`
+  display: flex;
+  border-bottom: 1px solid ${colors.gray};
+`
+const FieldName = styled.p`
+  font-weight: bold;
+  padding-right: 0.5rem;
+  flex: 1;
+  color: ${colors.primary};
+`
+const FieldValue = styled.p`
+  flex: 4;
+`
 // eslint-disable-next-line react/prop-types
 const ReviewAndSubmit = ({ steps }) => {
   const { name, email, interest, message } = steps
+
   return (
-    <div
-      style={{
-        width: '100%',
-        padding: '12px',
-        background: '#2ab640',
-      }}
-    >
-      <H4 style={{ color: 'white' }}>Summary</H4>
-      <table>
-        <tbody>
-          <tr>
-            <Td>Name</Td>
-            <Td>{name.value}</Td>
-          </tr>
-          <tr>
-            <Td>Email</Td>
-            <Td>{email.value}</Td>
-          </tr>
-          <tr>
-            <Td>Interest</Td>
-            <Td>{interest.value}</Td>
-          </tr>
-          <tr>
-            <Td>Message</Td>
-            <Td>{message ? message.value : 'Non'}</Td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <Wrapper>
+      <Title>Summary</Title>
+      <FieldsContainer>
+        <FieldGroup>
+          <FieldName>Name:</FieldName>
+          <FieldValue>{name.value}</FieldValue>
+        </FieldGroup>
+        <FieldGroup>
+          <FieldName>Email:</FieldName>
+          <FieldValue>{email.value}</FieldValue>
+        </FieldGroup>
+        <FieldGroup>
+          <FieldName>Interest:</FieldName>
+          <FieldValue>{interest.value}</FieldValue>
+        </FieldGroup>
+        <FieldGroup>
+          <FieldName>Message:</FieldName>
+          <FieldValue>{message.value && message.value.length > 50 ? message.value.slice(0, 50).concat('...') : message.value}</FieldValue>
+        </FieldGroup>
+      </FieldsContainer>
+    </Wrapper>
   )
 }
 ReviewAndSubmit.propTypes = {
