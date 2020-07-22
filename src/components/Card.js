d58038594c91e373b4f0af2a93bef32da6ef33cd
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2020-07-22 22:41:46 +0300

    style: add card to team members

--- src/components/Card.js
+++ src/components/Card.js
@@ -24,3 +24,18 @@ export const PolygonCard = styled.div`
     transform: skew(0);
   }
 `
+
+export const TeamCard = styled.div`
+  width: 300px;
+  padding: 50px 60px;
+  margin-right: 2rem;
+  background: #ffffff;
+  border: 1px solid #cccccc;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+
+  @media (max-width: 600px) {
+    margin-right: 0;
+    margin-bottom: 2rem;
+  }
+`
