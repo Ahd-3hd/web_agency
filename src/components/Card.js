f1b9a5d18aa60fa19e8be13980ec7533733c8fb9
Ahd <dafuq.ahd@gmail.com>
2020-07-26 02:13:55 +0300

    restructure the project content and switch from useTransition to useTrail to apply sliding animation to projects

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,6 @@ import styled from 'styled-components'
 export const Card = styled.div`
   margin: 1rem;
   padding: 1rem;
-  width: 100%;
   background: #ffffff;
   box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.2);
   border-radius: 0.4rem;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,82 +1,192 @@
+/* eslint-disable no-shadow */
+/* eslint-disable react/button-has-type */
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-lone-blocks */
+/* eslint-disable react/jsx-no-undef */
 import React, { useState, useCallback } from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
-import { useTransition, animated } from 'react-spring'
-import { H3, Body2, ListItem } from './typography'
-import { Card } from './Card'
+import { useTrail, config } from 'react-spring'
+
+import {
+  Wrapper,
+  SliderContainer,
+  CardImageContainer,
+  CardContentContainer,
+  ExtendedCard,
+  TechsContainer,
+  ExtendedH4,
+  ExtendedBody2,
+  ExtendedSpan,
+  AnimatedCard,
+} from './Portfolio.style'
 import { RectButton } from './Buttons'
-import { PortfolioSection, PortofiloList, Wrapper, Logo, Content, Span, ParnetSlider, SliderWrapper } from '../styles/portfolio'
+import exampleImage from '../images/example.jpg'
+import { ButtonText } from './typography'
+
+const AnimatedContainer = ({ distance }) => {
+  const trail = useTrail(3, {
+    config,
+    x: distance,
+    from: { x: 0 },
+  })
+  return trail.map(({ x, height, ...rest }, index) => (
+    <AnimatedCard style={{ ...rest, transform: x.interpolate(x => `translate3d(${x}%,0,0)`) }}>
+      <ExtendedCard>
+        <CardImageContainer>
+          <img src={exampleImage} alt="thumbnail" />
+        </CardImageContainer>
+        <CardContentContainer>
+          <ExtendedH4>Project Title</ExtendedH4>
+          <TechsContainer>
+            <ExtendedSpan>Techs: </ExtendedSpan>
+            <ExtendedSpan>JavaScript</ExtendedSpan>
+            <ExtendedSpan>JavaScript</ExtendedSpan>
+            <ExtendedSpan>JavaScript</ExtendedSpan>
+            <ExtendedSpan>JavaScript</ExtendedSpan>
+            <ExtendedSpan>JavaScript</ExtendedSpan>
+            <ExtendedSpan>JavaScript</ExtendedSpan>
+          </TechsContainer>
+          <ExtendedBody2>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa elementum, iaculis et, et. Aliquam nisl amet lorem quisque eu
+            commodo leo. Neque curabitur scelerisque dignissim ornare. Pellentesque turpis sed magna tempor lorem.
+          </ExtendedBody2>
+          <RectButton>
+            <ButtonText>View</ButtonText>
+          </RectButton>
+        </CardContentContainer>
+      </ExtendedCard>
+    </AnimatedCard>
+  ))
+}
 
 const Portfolio = () => {
-  const projects = [{ name: 'masgaz.com' }, { name: 'gsgapp.io' }]
+  const [distance, setDistance] = useState(0)
 
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "example.jpg" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
+  const handleSlide = d => {
+    if (d > 0 && distance < 0) {
+      setDistance(distance + d)
+    } else if (d < 0 && distance >= -100) {
+      setDistance(distance + d)
     }
-  `)
-  const [index, setIndex] = useState(0)
-  const onClick = useCallback(i => setIndex(i, []))
-  const transitions = useTransition(index, p => p, {
-    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
-    enter: { opacity: 1, transform: 'translate3d(50%,0,0)' },
-    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
-  })
-  // eslint-disable-next-line no-console
-  console.log(transitions.length, 'how many are there deearling')
+    console.log(distance, d)
+  }
+
   return (
-    <PortfolioSection>
-      <ParnetSlider>
-        <SliderWrapper>
-          {transitions.map(({ item, props, key }, i) => {
-            // eslint-disable-next-line no-console
-            console.log(item, 'the hell is this')
-            // eslint-disable-next-line no-console
-            console.log('i', i)
-            return (
-              <animated.div onClick={() => onClick(i)} key={key} style={props}>
-                <Card>
-                  <Wrapper>
-                    <Logo>
-                      <Img fluid={data.placeholderImage.childImageSharp.fluid} />
-                    </Logo>
-                    <Content>
-                      <H3>{projects[item].name}</H3>
-                      <Body2>
-                        <Span color="rgba(36, 36, 36, 0.6)">Technlogies:</Span>
-                        <Span>JavaScript</Span>
-                        <Span>JavaScript</Span>
-                        <Span>JavaScript</Span>
-                        <Span>JavaScript</Span>
-                      </Body2>
-                      <Body2>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa elementum, iaculis et, et. Aliquam nisl amet lorem
-                        quisque eu commodo leo. Neque curabitur scelerisque dignissim ornare. Pellentesque turpis sed magna tempor lorem.
-                      </Body2>
-                      <RectButton margin="0">Visit Website</RectButton>
-                    </Content>
-                  </Wrapper>
-                </Card>
-              </animated.div>
-            )
-          })}
-        </SliderWrapper>
-      </ParnetSlider>
-      <PortofiloList>
-        {projects.map(({ name }, i) => (
-          <ListItem selected={index === i} onClick={() => onClick(i)}>
-            {name}
-          </ListItem>
-        ))}
-      </PortofiloList>
-    </PortfolioSection>
+    <Wrapper>
+      <SliderContainer>
+        <AnimatedContainer distance={distance} />
+      </SliderContainer>
+      <div
+        style={{
+          position: 'absolute',
+        }}
+      >
+        <button onClick={() => handleSlide(100)}>Left</button>
+        <button onClick={() => handleSlide(-100)}>right</button>
+      </div>
+    </Wrapper>
   )
 }
 
 export default Portfolio
+
+{
+  /* <Card>
+<Wrapper>
+  <Logo>
+    <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  </Logo>
+  <Content>
+    <H3>This is Title</H3>
+    <Body2>
+      <Span color="rgba(36, 36, 36, 0.6)">Technlogies:</Span>
+      <Span>JavaScript</Span>
+      <Span>JavaScript</Span>
+      <Span>JavaScript</Span>
+      <Span>JavaScript</Span>
+    </Body2>
+    <Body2>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa elementum, iaculis et, et. Aliquam nisl amet lorem quisque eu
+      commodo leo. Neque curabitur scelerisque dignissim ornare. Pellentesque turpis sed magna tempor lorem.
+    </Body2>
+    <RectButton margin="0">Visit Website</RectButton>
+  </Content>
+</Wrapper>
+</Card> */
+}
+
+{
+  /* <ExtendedCard>
+<CardImageContainer>
+  <img src={exampleImage} />
+</CardImageContainer>
+<CardContentContainer>
+  <ExtendedH4>Project Title</ExtendedH4>
+  <TechsContainer>
+    <ExtendedSpan>Techs: </ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+  </TechsContainer>
+  <ExtendedBody2>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa elementum, iaculis et, et. Aliquam nisl amet lorem quisque eu
+    commodo leo. Neque curabitur scelerisque dignissim ornare. Pellentesque turpis sed magna tempor lorem.
+  </ExtendedBody2>
+  <RectButton>
+    <ButtonText>View</ButtonText>
+  </RectButton>
+</CardContentContainer>
+</ExtendedCard>
+<ExtendedCard>
+<CardImageContainer>
+  <img src={exampleImage} />
+</CardImageContainer>
+<CardContentContainer>
+  <ExtendedH4>Project Title</ExtendedH4>
+  <TechsContainer>
+    <ExtendedSpan>Techs: </ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+  </TechsContainer>
+  <ExtendedBody2>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa elementum, iaculis et, et. Aliquam nisl amet lorem quisque eu
+    commodo leo. Neque curabitur scelerisque dignissim ornare. Pellentesque turpis sed magna tempor lorem.
+  </ExtendedBody2>
+  <RectButton>
+    <ButtonText>View</ButtonText>
+  </RectButton>
+</CardContentContainer>
+</ExtendedCard>
+<ExtendedCard>
+<CardImageContainer>
+  <img src={exampleImage} />
+</CardImageContainer>
+<CardContentContainer>
+  <ExtendedH4>Project Title</ExtendedH4>
+  <TechsContainer>
+    <ExtendedSpan>Techs: </ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+    <ExtendedSpan>JavaScript</ExtendedSpan>
+  </TechsContainer>
+  <ExtendedBody2>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa elementum, iaculis et, et. Aliquam nisl amet lorem quisque eu
+    commodo leo. Neque curabitur scelerisque dignissim ornare. Pellentesque turpis sed magna tempor lorem.
+  </ExtendedBody2>
+  <RectButton>
+    <ButtonText>View</ButtonText>
+  </RectButton>
+</CardContentContainer>
+</ExtendedCard> */
+}
