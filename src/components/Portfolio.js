bf2b3a904012af70b0ed3404ce5527f7ba38f468
Ahd <dafuq.ahd@gmail.com>
2020-07-26 05:17:31 +0300

    remove commented code, remove comments that disable eslint errors and adhere to eslint rules, add the porfolio navigation button, remove the testing buttons,add title

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,7 @@
-/* eslint-disable no-shadow */
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable react/button-has-type */
-/* eslint-disable arrow-body-style */
-/* eslint-disable react/self-closing-comp */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-lone-blocks */
-/* eslint-disable react/jsx-no-undef */
-import React, { useState, useCallback } from 'react'
+/* eslint-disable no-shadow */
+import React, { useState } from 'react'
 import { useTrail, config } from 'react-spring'
 
 import {
@@ -19,10 +15,14 @@ import {
   ExtendedBody2,
   ExtendedSpan,
   AnimatedCard,
+  Title,
+  SliderNavigatorContainer,
+  SliderButton,
 } from './Portfolio.style'
 import { RectButton } from './Buttons'
 import exampleImage from '../images/example.jpg'
 import { ButtonText } from './typography'
+import ArrowIcon from '../static/ArrowIcon.svg'
 
 const AnimatedContainer = ({ distance }) => {
   const trail = useTrail(3, {
@@ -30,8 +30,8 @@ const AnimatedContainer = ({ distance }) => {
     x: distance,
     from: { x: 0 },
   })
-  return trail.map(({ x, height, ...rest }, index) => (
-    <AnimatedCard style={{ ...rest, transform: x.interpolate(x => `translate3d(${x}%,0,0)`) }}>
+  return trail.map(({ x, ...rest }, index) => (
+    <AnimatedCard style={{ ...rest, transform: x.interpolate(x => `translate3d(${x}%,0,0)`) }} key={index}>
       <ExtendedCard>
         <CardImageContainer>
           <img src={exampleImage} alt="thumbnail" />
@@ -49,7 +49,7 @@ const AnimatedContainer = ({ distance }) => {
           </TechsContainer>
           <ExtendedBody2>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa elementum, iaculis et, et. Aliquam nisl amet lorem quisque eu
-            commodo leo. Neque curabitur scelerisque dignissim ornare. Pellentesque turpis sed magna tempor lorem.
+            commodo leo. Neque curabitur scelerisque dignissim ornare.
           </ExtendedBody2>
           <RectButton>
             <ButtonText>View</ButtonText>
@@ -62,131 +62,42 @@ const AnimatedContainer = ({ distance }) => {
 
 const Portfolio = () => {
   const [distance, setDistance] = useState(0)
-
+  const [disableLeft, setDisableLeft] = useState(true)
+  const [disableRight, setDisableRight] = useState(false)
   const handleSlide = d => {
+    if (distance === 0) {
+      setDisableLeft(true)
+    } else {
+      setDisableLeft(false)
+    }
+    if (distance <= -200) {
+      setDisableRight(true)
+    } else {
+      setDisableRight(false)
+    }
     if (d > 0 && distance < 0) {
       setDistance(distance + d)
     } else if (d < 0 && distance >= -100) {
       setDistance(distance + d)
     }
-    console.log(distance, d)
   }
 
   return (
     <Wrapper>
+      <Title>Our Portfolio</Title>
       <SliderContainer>
         <AnimatedContainer distance={distance} />
       </SliderContainer>
-      <div
-        style={{
-          position: 'absolute',
-        }}
-      >
-        <button onClick={() => handleSlide(100)}>Left</button>
-        <button onClick={() => handleSlide(-100)}>right</button>
-      </div>
+      <SliderNavigatorContainer>
+        <SliderButton onClick={() => handleSlide(100)} left>
+          {!disableLeft && <ArrowIcon />}
+        </SliderButton>
+        <SliderButton onClick={() => handleSlide(-100)} right>
+          {!disableRight && <ArrowIcon />}
+        </SliderButton>
+      </SliderNavigatorContainer>
     </Wrapper>
   )
 }
 
 export default Portfolio
-
-{
-  /* <Card>
-<Wrapper>
-  <Logo>
-    <Img fluid={data.placeholderImage.childImageSharp.fluid} />
-  </Logo>
-  <Content>
-    <H3>This is Title</H3>
-    <Body2>
-      <Span color="rgba(36, 36, 36, 0.6)">Technlogies:</Span>
-      <Span>JavaScript</Span>
-      <Span>JavaScript</Span>
-      <Span>JavaScript</Span>
-      <Span>JavaScript</Span>
-    </Body2>
-    <Body2>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa elementum, iaculis et, et. Aliquam nisl amet lorem quisque eu
-      commodo leo. Neque curabitur scelerisque dignissim ornare. Pellentesque turpis sed magna tempor lorem.
-    </Body2>
-    <RectButton margin="0">Visit Website</RectButton>
-  </Content>
-</Wrapper>
-</Card> */
-}
-
-{
-  /* <ExtendedCard>
-<CardImageContainer>
-  <img src={exampleImage} />
-</CardImageContainer>
-<CardContentContainer>
-  <ExtendedH4>Project Title</ExtendedH4>
-  <TechsContainer>
-    <ExtendedSpan>Techs: </ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-  </TechsContainer>
-  <ExtendedBody2>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa elementum, iaculis et, et. Aliquam nisl amet lorem quisque eu
-    commodo leo. Neque curabitur scelerisque dignissim ornare. Pellentesque turpis sed magna tempor lorem.
-  </ExtendedBody2>
-  <RectButton>
-    <ButtonText>View</ButtonText>
-  </RectButton>
-</CardContentContainer>
-</ExtendedCard>
-<ExtendedCard>
-<CardImageContainer>
-  <img src={exampleImage} />
-</CardImageContainer>
-<CardContentContainer>
-  <ExtendedH4>Project Title</ExtendedH4>
-  <TechsContainer>
-    <ExtendedSpan>Techs: </ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-  </TechsContainer>
-  <ExtendedBody2>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa elementum, iaculis et, et. Aliquam nisl amet lorem quisque eu
-    commodo leo. Neque curabitur scelerisque dignissim ornare. Pellentesque turpis sed magna tempor lorem.
-  </ExtendedBody2>
-  <RectButton>
-    <ButtonText>View</ButtonText>
-  </RectButton>
-</CardContentContainer>
-</ExtendedCard>
-<ExtendedCard>
-<CardImageContainer>
-  <img src={exampleImage} />
-</CardImageContainer>
-<CardContentContainer>
-  <ExtendedH4>Project Title</ExtendedH4>
-  <TechsContainer>
-    <ExtendedSpan>Techs: </ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-    <ExtendedSpan>JavaScript</ExtendedSpan>
-  </TechsContainer>
-  <ExtendedBody2>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa elementum, iaculis et, et. Aliquam nisl amet lorem quisque eu
-    commodo leo. Neque curabitur scelerisque dignissim ornare. Pellentesque turpis sed magna tempor lorem.
-  </ExtendedBody2>
-  <RectButton>
-    <ButtonText>View</ButtonText>
-  </RectButton>
-</CardContentContainer>
-</ExtendedCard> */
-}
