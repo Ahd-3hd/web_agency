058b703bab8c2cda8e3a6d9681792efeece738e9
Ahd <dafuq.ahd@gmail.com>
2020-07-27 00:58:50 +0300

    create a container for team cards , create a title component, and align the components

--- src/styles/team.js
+++ src/styles/team.js
@@ -1,21 +1,32 @@
 import styled from 'styled-components'
+import { H2 } from '../components/typography'
+import { colors } from '../utils'
 
 export const TeamSection = styled.section`
   min-height: 100vh;
+  height: auto;
   display: flex;
-  align-items: center;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
   background-size: cover;
-
-  @media (max-width: 600px) {
-    flex-direction: column;
-    margin: 10rem 0;
-  }
+  padding: 2rem 0;
 `
 
+export const Title = styled(H2)`
+  color: ${colors.gray};
+`
+
+export const CardsContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(290px, 1fr));
+  grid-gap: 1rem;
+  justify-items: center;
+  padding: 0 6vw;
+`
 export const CardWrapper = styled.div`
-  width: 60%;
-  margin: 0 auto;
+  width: 100%;
 `
 
 export const JobTitle = styled.div`
