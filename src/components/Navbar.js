ec876a92f61b0973aace7a78a4438576a594fe36
Ahd <dafuq.ahd@gmail.com>
2020-07-28 05:20:58 +0300

    rearrange sections links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = ({ handleScroll }) => {
           setMenuActive(false)
         }}
       >
-        Portfolio
+        Services
       </MenuLink>
       <MenuLink
         onClick={() => {
@@ -29,7 +29,7 @@ const Navbar = ({ handleScroll }) => {
           setMenuActive(false)
         }}
       >
-        Our Team
+        Portfolio
       </MenuLink>
 
       <MenuLink
@@ -38,7 +38,7 @@ const Navbar = ({ handleScroll }) => {
           setMenuActive(false)
         }}
       >
-        Services
+        Team
       </MenuLink>
       <MenuLink
         onClick={() => {
