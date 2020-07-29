c54eff6d05a1e2aed84eccd5aa51fa989bdb1f9e
amanshawar <amanshawar_a@hotmail.com>
2020-07-29 18:14:50 +0300

    add google analtyics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,15 @@ module.exports = {
     author: `@zaatdev`,
   },
   plugins: [
+    {
+  resolve: `gatsby-plugin-google-gtag`,
+    options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+      trackingIds: [
+          "UA-173974064-1", // Google Analytics / GA
+      ]
+    }
+  },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
