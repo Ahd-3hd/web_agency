6515d4cffa0b4cabe674ecf231e47cf9a5c6f666
Ahd <dafuq.ahd@gmail.com>
2020-07-29 15:23:42 +0300

    shuffle team members array

--- src/components/Team.js
+++ src/components/Team.js
@@ -10,6 +10,47 @@ import { H5 } from './typography'
 import GitHubIcon from '../static/GitHub.svg'
 import LinkedInIcon from '../static/LinkedIn.svg'
 
+function shuffleArray(array) {
+  // eslint-disable-next-line no-plusplus
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    // eslint-disable-next-line no-param-reassign
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+  return array
+}
+
+const members = [
+  {
+    name: 'Ghassan',
+    img: 'ghassanImage',
+    title: 'Team Lead',
+    github: 'Ghassan',
+    linkdin: 'Ghassan',
+  },
+  {
+    name: 'Yosef',
+    img: 'yosefImage',
+    title: 'Team Lead',
+    github: 'Yosef',
+    linkdin: 'Yosef',
+  },
+  {
+    name: 'Aman',
+    img: 'amanImage',
+    title: 'Team Lead',
+    github: 'Aman',
+    linkdin: 'Aman',
+  },
+  {
+    name: 'Ahd',
+    img: 'ahdImage',
+    title: 'Team Lead',
+    github: 'Ahd',
+    linkdin: 'Ahd',
+  },
+]
+
 const Team = ({ refs }) => {
   const data = useStaticQuery(graphql`
     query {
@@ -48,94 +89,36 @@ const Team = ({ refs }) => {
     <TeamSection ref={refs} id="team">
       <Title>Our Team</Title>
       <CardsContainer>
-        <TeamCard>
-          <Img fluid={data.ghassanImage.childImageSharp.fluid} />
-          <H5
-            css={css`
-              text-align: center;
-              margin-top: 1rem;
-              margin-bottom: 0.5rem;
-            `}
-          >
-            Ghassan
-          </H5>
-          <JobTitle>Team Lead</JobTitle>
-          <SocialBox>
-            <GitHubIcon />
-            <Link href="github.com">@ghassan</Link>
-          </SocialBox>
-          <SocialBox>
-            <LinkedInIcon />
-            <Link href="github.com">@ghassan</Link>
-          </SocialBox>
-        </TeamCard>
-        <TeamCard>
-          <Img fluid={data.yosefImage.childImageSharp.fluid} />
-          <H5
-            css={css`
-              text-align: center;
-              margin-top: 1rem;
-              margin-bottom: 0.5rem;
-            `}
-          >
-            Yosef
-          </H5>
-          <JobTitle>Team Lead</JobTitle>
-          <SocialBox>
-            <GitHubIcon />
-            <Link href="github.com">@ghassan</Link>
-          </SocialBox>
-          <SocialBox>
-            <LinkedInIcon />
-            <Link href="github.com">@ghassan</Link>
-          </SocialBox>
-        </TeamCard>
-        <TeamCard>
-          <Img fluid={data.amanImage.childImageSharp.fluid} />
-          <H5
-            css={css`
-              text-align: center;
-              margin-top: 1rem;
-              margin-bottom: 0.5rem;
-            `}
-          >
-            Aman
-          </H5>
-          <JobTitle>Team Lead</JobTitle>
-          <SocialBox>
-            <GitHubIcon />
-            <Link href="github.com">@ghassan</Link>
-          </SocialBox>
-          <SocialBox>
-            <LinkedInIcon />
-            <Link href="github.com">@ghassan</Link>
-          </SocialBox>
-        </TeamCard>
-        <TeamCard>
-          <Img fluid={data.ahdImage.childImageSharp.fluid} />
-          <H5
-            css={css`
-              text-align: center;
-              margin-top: 1rem;
-              margin-bottom: 0.5rem;
-            `}
-          >
-            Ahd
-          </H5>
-          <JobTitle>Team Lead</JobTitle>
-          <SocialBox>
-            <GitHubIcon />
-            <Link href="github.com">@ghassan</Link>
-          </SocialBox>
-          <SocialBox>
-            <LinkedInIcon />
-            <Link href="github.com">@ghassan</Link>
-          </SocialBox>
-        </TeamCard>
+        {shuffleArray(members).map(({ name, img, title, github, linkdin }) => (
+          <TeamCard>
+            <Img fluid={data[img].childImageSharp.fluid} />
+            <H5
+              css={css`
+                text-align: center;
+                margin-top: 1rem;
+                margin-bottom: 0.5rem;
+              `}
+            >
+              {name}
+            </H5>
+            <JobTitle>{title}</JobTitle>
+            <SocialBox>
+              <GitHubIcon />
+              {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+              <Link href={`https://github.com/${github}`}>@{github}</Link>
+            </SocialBox>
+            <SocialBox>
+              <LinkedInIcon />
+              {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+              <Link href={`https://github.com/${linkdin}`}>@{linkdin}</Link>
+            </SocialBox>
+          </TeamCard>
+        ))}
       </CardsContainer>
     </TeamSection>
   )
 }
+
 Team.propTypes = {
   refs: PropTypes.shape({ current: PropTypes.elementType }).isRequired,
 }
