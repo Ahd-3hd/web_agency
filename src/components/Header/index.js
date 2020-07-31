b4098793c68d48fd8f46686c15fd4dc03564701f
Ahd <dafuq.ahd@gmail.com>
2020-07-31 09:33:05 +0300

    make the gauges values dynamic and change automatically

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+/* eslint-disable no-unused-expressions */
+import React, { useState, useEffect } from 'react'
 import {
   Wrapper,
   LeftContainer,
@@ -18,36 +19,59 @@ import TwitterIcon from '../../static/TwitterIcon.svg'
 import InstagramIcon from '../../static/InstagramIcon.svg'
 import Gauge from '../Gauge'
 
-const Header = () => (
-  <Wrapper>
-    <LeftContainer>
-      <Logo>ZAAT.DEV</Logo>
-      <Title>Keeping your customers engaged and happier than ever!</Title>
-      <Button>CONTACT</Button>
-      <SocialContainer>
-        <SocialLinksContainer>
-          <SocialLink href="#">
-            <FacebookIcon />
-          </SocialLink>
-          <SocialLink href="#">
-            <TwitterIcon />
-          </SocialLink>
-          <SocialLink href="#">
-            <InstagramIcon />
-          </SocialLink>
-        </SocialLinksContainer>
-        <SocialText>@zaatdev</SocialText>
-      </SocialContainer>
-    </LeftContainer>
-    <RightContainer>
-      <RightInnerContainer>
-        <RightCard />
-        <Gauge value="70" label="PERFORMANCE" top="-60%" left="15%" />
-        <Gauge value="70" label="SEO" top="-70%" left="60%" />
-        <Gauge value="70" label="Quality" top="110%" left="25%" />
-      </RightInnerContainer>
-    </RightContainer>
-  </Wrapper>
-)
+const Header = () => {
+  const [performance, setPerformance] = useState(0)
+  const [seo, setSeo] = useState(0)
+  const [quality, setQuality] = useState(0)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      performance > 100 ? setPerformance(0) : setPerformance(performance + 1)
+    }, 50)
+    return () => clearTimeout(timer)
+  }, [performance])
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      seo > 100 ? setSeo(0) : setSeo(seo + 1)
+    }, 150)
+    return () => clearTimeout(timer)
+  }, [seo])
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      quality > 100 ? setQuality(0) : setQuality(quality + 1)
+    }, 10)
+    return () => clearTimeout(timer)
+  }, [quality])
+  return (
+    <Wrapper>
+      <LeftContainer>
+        <Logo>ZAAT.DEV</Logo>
+        <Title>Keeping your customers engaged and happier than ever!</Title>
+        <Button>CONTACT</Button>
+        <SocialContainer>
+          <SocialLinksContainer>
+            <SocialLink href="#">
+              <FacebookIcon />
+            </SocialLink>
+            <SocialLink href="#">
+              <TwitterIcon />
+            </SocialLink>
+            <SocialLink href="#">
+              <InstagramIcon />
+            </SocialLink>
+          </SocialLinksContainer>
+          <SocialText>@zaatdev</SocialText>
+        </SocialContainer>
+      </LeftContainer>
+      <RightContainer>
+        <RightInnerContainer>
+          <RightCard />
+          <Gauge value={performance} label="PERFORMANCE" top="-60%" left="15%" />
+          <Gauge value={seo} label="SEO" top="-70%" left="60%" />
+          <Gauge value={quality} label="QUALITY" top="110%" left="25%" />
+        </RightInnerContainer>
+      </RightContainer>
+    </Wrapper>
+  )
+}
 
 export default Header
