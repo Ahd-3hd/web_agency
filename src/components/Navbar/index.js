3224a2d39ea49b8d83370ca3e0a6162c5f346f74
Ahd <dafuq.ahd@gmail.com>
2020-07-31 07:44:58 +0300

    create and style the navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Wrapper, MenuButton, LogoContainer, Overlay, MenuLink } from './index.style'
+import MenuIcon from '../../static/MenuIcon.svg'
 
-const Navbar = () => <div>Navbar</div>
+const Navbar = () => {
+  const [isMenuActive, setIsMenuActive] = useState(false)
+  return (
+    <Wrapper>
+      <LogoContainer>ZAAT.DEV</LogoContainer>
+      <MenuButton onClick={() => setIsMenuActive(!isMenuActive)}>
+        <MenuIcon />
+      </MenuButton>
+      <Overlay isMenuActive={isMenuActive}>
+        <MenuLink href="#" isMenuActive={isMenuActive} order={1}>
+          Home
+        </MenuLink>
+        <MenuLink href="#" isMenuActive={isMenuActive} order={2}>
+          Portfolio
+        </MenuLink>
+        <MenuLink href="#" isMenuActive={isMenuActive} order={3}>
+          Services
+        </MenuLink>
+        <MenuLink href="#" isMenuActive={isMenuActive} order={4}>
+          Contact
+        </MenuLink>
+        <MenuLink href="#" isMenuActive={isMenuActive} order={5}>
+          Team
+        </MenuLink>
+      </Overlay>
+    </Wrapper>
+  )
+}
 
 export default Navbar
