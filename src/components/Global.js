faf46afb0f6de952748d0110e7366cf8b863bfbf
Ahd <dafuq.ahd@gmail.com>
2020-07-31 08:25:12 +0300

    add background to body

--- src/components/Global.js
+++ src/components/Global.js
@@ -1,11 +1,20 @@
 import { createGlobalStyle } from 'styled-components'
 import './fonts.css'
 import { fonts } from '../utils'
+import backgroundImageTransparent from '../images/backgroundtransparent.svg'
+import backgroundImage from '../images/background.svg'
 
 const GlobalStyles = createGlobalStyle`
   body{
+    background:url(${backgroundImageTransparent}) no-repeat;
+    background-size:contain;
+    background-position:center ;
+    background-attachment:fixed;
     margin:0;
-    font-family:${fonts.primary}
+    font-family:${fonts.primary};
+    @media (min-width:1280px){
+    background:url(${backgroundImage}) no-repeat;
+    }
   }
   *{
     box-sizing:border-box;
