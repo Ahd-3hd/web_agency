9ffc17c0eedcfe8378b78ea3ff2d01c14c5aec52
Ahd <dafuq.ahd@gmail.com>
2020-07-31 08:25:37 +0300

    render header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import SEO from '../components/Seo'
+import Header from '../components/Header'
 
 const IndexPage = () => (
   <>
     <Layout>
       <SEO title="Home" />
+      <Header />
     </Layout>
   </>
 )
