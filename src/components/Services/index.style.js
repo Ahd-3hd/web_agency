22f5f290888e630a38da65cfd696b274092bf049
Ahd <dafuq.ahd@gmail.com>
2020-08-01 14:31:16 +0300

    shift layout for desktop and large screens

--- src/components/Services/index.style.js
+++ src/components/Services/index.style.js
@@ -5,22 +5,45 @@ export const Wrapper = styled.section`
   display: flex;
   flex-direction: column-reverse;
   align-items: center;
+  @media (min-width: 1440px) {
+    flex-direction: row;
+  }
 `
-export const TitleContainer = styled.div``
+export const TitleContainer = styled.div`
+  @media (min-width: 1440px) {
+    flex: 1;
+  }
+`
 export const Title = styled.h2`
   color: ${colors.black};
   text-shadow: 0px 0px 15px rgba(0, 0, 0, 0.25);
   font-size: 2rem;
   text-align: center;
   max-width: 20ch;
+  @media (min-width: 1440px) {
+    font-size: 7vw;
+    text-align: left;
+  }
 `
-export const ServicesContainer = styled.div``
+export const ServicesContainer = styled.div`
+  @media (min-width: 1440px) {
+    flex: 2;
+  }
+`
 export const Service = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 1.5rem 0.5rem;
   margin: 1rem 0;
+  @media (min-width: 768px) {
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+  @media (min-width: 1440px) {
+    max-width: 90ch;
+  }
 `
 export const ServiceImage = styled.div`
   background: ${colors.purple} url(${({ bg }) => bg}) no-repeat;
@@ -30,6 +53,13 @@ export const ServiceImage = styled.div`
   height: 40vw;
   border-radius: 1rem;
   box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.25);
+  @media (min-width: 768px) {
+    width: 200px;
+    height: 150px;
+  }
+  @media (min-width: 1440px) {
+    background-size: 60%;
+  }
 `
 export const ServiceContent = styled.div`
   display: flex;
@@ -38,6 +68,11 @@ export const ServiceContent = styled.div`
   text-align: center;
   > p {
     margin: 0.7rem 0;
+  }
+  @media (min-width: 768px) {
+    align-items: flex-start;
+    margin-left: 1rem;
+    text-align: left;
   }
 `
 export const ServiceTitle = styled.p`
